aa6f58d64629f8280fea61a78929251ec308a567
Gonen <Goneni45@gmail.com>
2025-12-13 19:42:20 +0200

    1

--- src/assignments/Ex2/Index2D.java
+++ src/assignments/Ex2/Index2D.java
@@ -1,41 +1,44 @@
 package assignments.Ex2;
 
+import classes.week6.Point2D;
+
 public class Index2D implements Pixel2D {
+
+    private int _h;
+    private int _w;
+
     public Index2D(int w, int h) {
-        ;
+        _h=Math.abs(h); //abs is needed???
+        _w=Math.abs(w);
     }
     public Index2D(Pixel2D other) {
-        ;
+//        copy constructor
+        this._h=other.getY();
+        this._w=other.getX();
     }
     @Override
-    public int getX() {
-
-        return 0;
-    }
+    public int getX() {return _w;}
 
     @Override
-    public int getY() {
-
-        return 0;
-    }
+    public int getY() {return _h;}
 
     @Override
     public double distance2D(Pixel2D p2) {
-
-        return 0;
+//        if p2 is null{}
+        return Math.sqrt( (this._w * p2.getX()) + (this._h * p2.getY()) );
     }
 
     @Override
     public String toString() {
-        String ans = null;
+        String ans = "("+this._w+","+this._h+")";
 
         return ans;
     }
 
     @Override
     public boolean equals(Object p) {
-        boolean ans = true;
-
-        return ans;
+        if(p==null || !(p instanceof Index2D)) {return false;}
+        Index2D p2 = (Index2D)p;
+        return (((this._h==p2.getY()) && (this._w==p2.getX())));
     }
 }
