1690e90b90bcfb1af69d31bacd776cb0fa74ae9e
Gonen <Goneni45@gmail.com>
2025-12-21 19:40:51 +0200

    fill

--- src/assignments/Ex2/Map.java
+++ src/assignments/Ex2/Map.java
@@ -1,9 +1,6 @@
 package assignments.Ex2;
-import classes.week6.Point2D;
 
 import java.io.Serializable;
-
-import static jdk.internal.org.jline.utils.Colors.h;
 
 /**
  * This class represents a 2D map (int[w][h]) as a "screen" or a raster matrix or maze over integers.
@@ -12,119 +9,149 @@ import static jdk.internal.org.jline.utils.Colors.h;
  * @author boaz.benmoshe
  *
  */
-public class Map implements Map2D, Serializable{
+public class Map implements Map2D, Serializable {
     private int[][] _map;
+    private int counter = 0;
 
     // edit this class below
-	/**
-	 * Constructs a w*h 2D raster map with an init value v.
-	 * @param w
-	 * @param h
-	 * @param v
-	 */
+
+    /**
+     * Constructs a w*h 2D raster map with an init value v.
+     *
+     * @param w
+     * @param h
+     * @param v
+     */
 
 
-	public Map(int w, int h, int v) {init(w, h, v);}
-	/**
-	 * Constructs a square map (size*size).
-	 * @param size
-	 */
-	public Map(int size) {this(size,size, 0);}
-	
-	/**
-	 * Constructs a map from a given 2D array.
-	 * @param data
-	 */
-	public Map(int[][] data) {
-		init(data);
-	}
-	@Override
-	public void init(int w, int h, int v) {
+    public Map(int w, int h, int v) {
+        init(w, h, v);
+    }
+
+    /**
+     * Constructs a square map (size*size).
+     *
+     * @param size
+     */
+    public Map(int size) {
+        this(size, size, 0);
+    }
+
+    /**
+     * Constructs a map from a given 2D array.
+     *
+     * @param data
+     */
+    public Map(int[][] data) {
+        init(data);
+    }
+
+    @Override
+    public void init(int w, int h, int v) {
         _map = new int[h][w];
-        for (int i=0; i<h; i++){
-            for(int j=0; j<w; j++){
-                _map[i][j]=v;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                _map[i][j] = v;
             }
         }
 
-	}
-	@Override
-	public void init(int[][] arr) {
-        if ((arr== null)||(arr.length == 0)) {throw new RuntimeException("array cannot be null or with h=0 ");}
-        int h= arr.length;
+    }
+
+    @Override
+    public void init(int[][] arr) {
+        if ((arr == null) || (arr.length == 0)) {
+            throw new RuntimeException("array cannot be null or with h=0 ");
+        }
+        int h = arr.length;
         int w;
-        if (arr[0].length>0 ){
-         w=arr[0].length;}
-        else {throw new RuntimeException("w most be larger then 0");}
+        if (arr[0].length > 0) {
+            w = arr[0].length;
+        } else {
+            throw new RuntimeException("w most be larger then 0");
+        }
 
         _map = new int[h][w];
-        for (int i=0; i<h; i++){
+        for (int i = 0; i < h; i++) {
             if (arr[i].length != w) {
                 throw new RuntimeException("Array must have same length");
             }
-            for(int j=0; j<w; j++){
+            for (int j = 0; j < w; j++) {
                 _map[i][j] = arr[i][j];
             }
         }
 
-	}
-	@Override
-	public int[][] getMap() {
-        int h= _map.length;
-        int w=_map[0].length;
-		int[][] ans = new int[h][w];
-        for(int r=0; r<h; r++){
-            for(int c=0; c<w; c++){//[2][1] arr[2] in place 1
-                ans[r][c]=_map[r][c];
+    }
+
+    @Override
+    public int[][] getMap() {
+        int h = _map.length;
+        int w = _map[0].length;
+        int[][] ans = new int[h][w];
+        for (int r = 0; r < h; r++) {
+            for (int c = 0; c < w; c++) {//[2][1] arr[2] in place 1
+                ans[r][c] = _map[r][c];
             }
         }
-		return ans;
-	}
-	@Override
-	public int getWidth() {
+        return ans;
+    }
+
+    @Override
+    public int getWidth() {
         int ans = _map[0].length;
         return ans;
     }
-	@Override
-	public int getHeight() {
-        int ans = _map.length ;
+
+    @Override
+    public int getHeight() {
+        int ans = _map.length;
         return ans;
     }
-	@Override
-	public int getPixel(int x, int y) {
+
+    @Override
+    public int getPixel(int x, int y) {
         Index2D p = new Index2D(x, y);
-        if(isInside(p)){
-        int ans =_map[y][x];
-        return ans;}
-        else{throw new RuntimeException("x or y out of dim"); }
-    }
-	@Override
-	public int getPixel(Pixel2D p) {
-        if(isInside(p)){
-        int ans =_map[p.getY()][p.getX()];
-        return ans;}
-        else{throw new RuntimeException("x or y out of dim"); }
-	}
-	@Override
-	public void setPixel(int x, int y, int v) {//row-y  col-x
-        Index2D p = new Index2D(x, y);
-        if(isInside(p)){
-            _map[y][x]=v;
+        if (isInside(p)) {
+            int ans = _map[y][x];
+            return ans;
+        } else {
+            throw new RuntimeException("x or y out of dim");
         }
-        else{throw new RuntimeException("x or y out of dim"); }
+    }
+
+    @Override
+    public int getPixel(Pixel2D p) {
+        if (isInside(p)) {
+            int ans = _map[p.getY()][p.getX()];
+            return ans;
+        } else {
+            throw new RuntimeException("x or y out of dim");
+        }
+    }
+
+    @Override
+    public void setPixel(int x, int y, int v) {//row-y  col-x
+        Index2D p = new Index2D(x, y);
+        if (isInside(p)) {
+            _map[y][x] = v;
+        } else {
+            throw new RuntimeException("x or y out of dim");
+        }
 
     }
-	@Override
-	public void setPixel(Pixel2D p, int v) {
-        int x=p.getX(),y=p.getY();
-        if(isInside(p)){
-            _map[y][x]=v;}
-        else{throw new RuntimeException("x or y out of dim"); }
+
+    @Override
+    public void setPixel(Pixel2D p, int v) {
+        int x = p.getX(), y = p.getY();
+        if (isInside(p)) {
+            _map[y][x] = v;
+        } else {
+            throw new RuntimeException("x or y out of dim");
+        }
     }
 
     @Override
     public boolean isInside(Pixel2D p) {
-        boolean ans =  (p.getY() < this.getHeight()) && (p.getX() < this.getWidth());
+        boolean ans = (p.getY() < this.getHeight()) && (p.getX() < this.getWidth());
         return ans;
     }
 
@@ -136,10 +163,10 @@ public class Map implements Map2D, Serializable{
 
     @Override
     public void addMap2D(Map2D p) {
-        if(this.sameDimensions(p)){
-            for(int r=0;r<p.getHeight();r++){
-                for(int c=0; c<p.getWidth();c++){
-                    this.setPixel(r,c,(this.getPixel(r,c)+p.getPixel(r,c)));
+        if (this.sameDimensions(p)) {
+            for (int r = 0; r < p.getHeight(); r++) {
+                for (int c = 0; c < p.getWidth(); c++) {
+                    this.setPixel(c, r, (this.getPixel(c, r) + p.getPixel(c, r)));
                 }
             }
         }
@@ -148,35 +175,40 @@ public class Map implements Map2D, Serializable{
 
     @Override
     public void mul(double scalar) {
-        for(int r=0;r<this.getHeight();r++) {
+        for (int r = 0; r < this.getHeight(); r++) {
             for (int c = 0; c < this.getWidth(); c++) {
-                this.setPixel(r, c, (int) (this.getPixel(r, c) * scalar));
+                this.setPixel(c, r, (int) (this.getPixel(c, r) * scalar));
             }
         }
     }
 
     @Override
     public void rescale(double sx, double sy) {
-        int new_h= (int) (this.getHeight()* sy), new_w= (int) (this.getWidth()* sx);
-        int[][] re_map= new int[new_h][new_w];
-        for (int r=0; r<new_h; r++){
-            for(int c=0; c < new_w ; c++){
-                int old_w= (int)(c/sx),old_h= (int)(r/sy);
-                re_map[r][c]=this.getPixel(old_w,old_h);
+        int new_h = (int) (this.getHeight() * sy), new_w = (int) (this.getWidth() * sx);
+        int[][] re_map = new int[new_h][new_w];
+        for (int r = 0; r < new_h; r++) {
+            for (int c = 0; c < new_w; c++) {
+                int old_w = (int) (c / sx), old_h = (int) (r / sy);
+                re_map[r][c] = this.getPixel(old_w, old_h);
             }
         }
-       this.init(re_map);
+        this.init(re_map);
 
     }
 
     @Override
     public void drawCircle(Pixel2D center, double rad, int color) {
-        for(int r=(int) (center.getY()-rad); r<(int) (center.getY()+rad);r++){
-            for(int c=(int) (center.getX()-rad); c<(int) (center.getX()+rad);c++){
-                Index2D point= new Index2D(c,r);//Can be improved
-                if (CircleContains(point,center,rad)){
-                    this.setPixel(point,color);
-               }
+        if (rad <= 0) {
+            throw new IllegalArgumentException("rad must be bigger than 0");
+        }
+        for (int r = (int) (center.getY() - rad); r < (int) (center.getY() + rad); r++) {
+            for (int c = (int) (center.getX() - rad); c < (int) (center.getX() + rad); c++) {
+                Index2D point = new Index2D(c, r);
+                if (CircleContains(point, center, rad)) {
+                    if (this.isInside(point)) {
+                        this.setPixel(point, color);
+                    }
+                }
             }
         }
     }
@@ -197,26 +229,25 @@ public class Map implements Map2D, Serializable{
                 return;
             }
 
-            double slope = (double)(y2 - y1) / (x2 - x1),y = y1;
+            double slope = (double) (y2 - y1) / (x2 - x1), y = y1;
 
             for (int x = x1; x <= x2; x++) {
-                int ry = (int)Math.round(y);
+                int ry = (int) Math.round(y);
                 if (this.isInside(new Index2D(x, ry))) {
                     setPixel(x, ry, color);
                 }
                 y += slope;
             }
-        }
-        else {
+        } else {
             if (y1 > y2) {
                 drawLine(p2, p1, color);
                 return;
             }
 
-            double slope = (double)(x2 - x1) / (y2 - y1), x = x1;
+            double slope = (double) (x2 - x1) / (y2 - y1), x = x1;
 
             for (int y = y1; y <= y2; y++) {
-                int rx = (int)Math.round(x);
+                int rx = (int) Math.round(x);
                 if (this.isInside(new Index2D(rx, y))) {
                     setPixel(rx, y, color);
                 }
@@ -227,54 +258,98 @@ public class Map implements Map2D, Serializable{
 
     @Override
     public void drawRect(Pixel2D p1, Pixel2D p2, int color) {
-        int x_start=Math.min(p1.getX(),p2.getX()), x_end=Math.max(p1.getX(),p2.getX());
-        int y_start=Math.min(p1.getY(),p2.getY()), y_end=Math.max(p1.getY(),p2.getY());
-        for(int r=y_start; r<=y_end;r++){
-            Index2D p_start= new Index2D(x_start,r), p_end= new Index2D(x_end, r);
-            drawLine(p_start,p_end,color);
+        int x_start = Math.min(p1.getX(), p2.getX()), x_end = Math.max(p1.getX(), p2.getX());
+        int y_start = Math.min(p1.getY(), p2.getY()), y_end = Math.max(p1.getY(), p2.getY());
+        for (int r = y_start; r <= y_end; r++) {
+            Index2D p_start = new Index2D(x_start, r), p_end = new Index2D(x_end, r);
+            drawLine(p_start, p_end, color);
         }
     }
 
     @Override
     public boolean equals(Object ob) {
         boolean ans = false;
+        if (ob == null || !(ob instanceof Map2D)) {
+            return ans;
+        }
+        Map2D map2 = (Map2D) ob;
+        if (!(this.sameDimensions(map2))) {
+            return ans;
+        }
+        for (int r = 0; r < this.getHeight(); r++) {
+            for (int c = 0; c < this.getWidth(); c++) {
+                if (this.getPixel(c, r) != map2.getPixel(c, r)) {
+                    return ans;
+                }
+            }
+        }
+        ans = true;
+        return ans;
+    }
+
+    @Override
+    /**
+     * Fills this map with the new color (new_v) starting from p.
+     * https://en.wikipedia.org/wiki/Flood_fill
+     */
+    public int fill(Pixel2D xy, int new_v, boolean cyclic) {
+        this.counter = 0;
+        int color = this.getPixel(xy);
+
+        if (color != new_v && isInside(xy)) {
+            filler(xy, color, new_v, cyclic);
+        }
+        return this.counter;
+    }
+
+    @Override
+    /**
+     * BFS like shortest the computation based on iterative raster implementation of BFS, see:
+     * https://en.wikipedia.org/wiki/Breadth-first_search
+     */
+    public Pixel2D[] shortestPath(Pixel2D p1, Pixel2D p2, int obsColor, boolean cyclic) {
+        Pixel2D[] ans = null;  // the result.
 
         return ans;
     }
-	@Override
-	/** 
-	 * Fills this map with the new color (new_v) starting from p.
-	 * https://en.wikipedia.org/wiki/Flood_fill
-	 */
-	public int fill(Pixel2D xy, int new_v,  boolean cyclic) {
-		int ans = -1;
 
-		return ans;
-	}
-
-	@Override
-	/**
-	 * BFS like shortest the computation based on iterative raster implementation of BFS, see:
-	 * https://en.wikipedia.org/wiki/Breadth-first_search
-	 */
-	public Pixel2D[] shortestPath(Pixel2D p1, Pixel2D p2, int obsColor, boolean cyclic) {
-		Pixel2D[] ans = null;  // the result.
-
-		return ans;
-	}
     @Override
     public Map2D allDistance(Pixel2D start, int obsColor, boolean cyclic) {
         Map2D ans = null;  // the result.
 
         return ans;
     }
-    ////////////////////// Private Methods ///////////////////////
-    private boolean CircleContains(Pixel2D p,Pixel2D center, double r) {
+
+    /// /////////////////// Private Methods ///////////////////////
+    private boolean CircleContains(Pixel2D p, Pixel2D center, double r) {
         return r >= p.distance2D(center);
     }
 
+    private void filler(Pixel2D p, int old_color, int new_v, boolean cyclic) {
+        int x =p.getX(), y=p.getY();
+        if (cyclic) {
+            int w =getWidth();
+            int h =getHeight();
+            x = (x+w)%w;
+            y = (y+h)%h;
+            p = new Index2D(x,y);
+        } else {
+            if (!isInside(p)) {return;}
+        }
 
+        if (getPixel(p) != old_color) {return;}
 
+        setPixel(p, new_v);
+        this.counter++;
+
+        filler(new Index2D(x, y + 1), old_color, new_v, cyclic);
+        filler(new Index2D(x, y - 1), old_color, new_v, cyclic);
+        filler(new Index2D(x + 1, y), old_color, new_v, cyclic);
+        filler(new Index2D(x - 1, y), old_color, new_v, cyclic);
+    }
 }
+
+
+
 
 
