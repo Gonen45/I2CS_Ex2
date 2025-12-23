42d9887dd9ea9e33a577265e9a026ac206a50677
Gonen <Goneni45@gmail.com>
2025-12-23 23:06:50 +0200

    more tests of map

--- src/assignments/Ex2/MapTest.java
+++ src/assignments/Ex2/MapTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Timeout;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Intro2CS, 2026A, this is a very
  */
@@ -14,6 +16,7 @@ class MapTest {
      */
     private int[][] _map_3_3 = {{0,1,0}, {1,0,1}, {0,1,0}};
     private Map2D _m0, _m1, _m3_3;
+    private Index2D p = new Index2D(0,0);
     @BeforeEach
     public void setuo() {
         _m1=new Map(100);
@@ -69,8 +72,8 @@ class MapTest {
         @Test
         void getPixel() {
             _m0 =new Map(9,4,1);
-            int p= _m0.getPixel(0,0);
-            assertEquals(1, p);
+            int i= _m0.getPixel(0,0);
+            assertEquals(1, i);
 
         }
 
@@ -82,33 +85,76 @@ class MapTest {
         void setPixel() {
             _m0 =new Map(9,4,1);
             _m0.setPixel(0,0,8);
-            int p= _m0.getPixel(0,0);
-            assertEquals(8, p);
+            int i= _m0.getPixel(0,0);
+            assertEquals(8, i);
 
         }
 
         @Test
-        void testSetPixel() {
+        void testSetPixel() {//TODO
         }
 
         @Test
         void isInside() {
+        assertTrue(_m0.isInside(p));
+
+        _m0=new Map(100);
+        Index2D p2= new Index2D(99,99);
+        assertTrue(_m0.isInside(p2));
+
+        p.change(-1,4);
+        assertFalse(_m0.isInside(p));
         }
 
         @Test
         void sameDimensions() {
+            _m1=new Map(80,80,1);
+            _m0=new Map(80,80,8);
+            assertTrue(_m0.sameDimensions(_m1));
+
+            _m1=new Map(80);
+            _m0=new Map(8);
+            assertFalse(_m0.sameDimensions(_m1));
+
         }
 
         @Test
         void addMap2D() {
+            _m1=new Map(80,80,1);
+            _m0=new Map(80,80,-1);
+            _m0.addMap2D(_m1);
+
+            _m1=new Map(80);//recreated to map of zeros
+            assertEquals(_m0,_m1);
+
         }
 
         @Test
         void mul() {
+            _m1=new Map(80,80,2);
+            _m0=new Map(80,80,4);
+            _m1.mul(2);
+            assertEquals(_m0,_m1);
+
+            _m1=new Map(80,80,2);
+            _m0=new Map(80,80,2);
+            _m1.mul(1.4);
+            assertEquals(_m0,_m1);
+
         }
 
         @Test
         void rescale() {
+            int[][] data = {
+                    {10, 10, 20, 20},
+                    {10, 10, 20, 20},
+                    {30, 20, 40, 40},
+                    {30, 30, 40, 40}};
+            _m0 = new Map(data);
+            _m0.rescale(0.5, 0.5);
+            int i=_m0.getPixel(1,1);
+            assertEquals(40,i);
+
         }
 
         @Test
@@ -117,6 +163,22 @@ class MapTest {
 
         @Test
         void drawLine() {
+            _m0=new Map(100);
+            int r=_m0.getHeight()/2;
+            p.change(0,r);
+            Index2D p2= new Index2D(99,r);
+            _m0.drawLine(p2,p,4);
+            int n = (int) (Math.random() * 100);
+
+            int i=_m0.getPixel(n,r);
+            assertEquals(4,i);
+
+            n = (int) (Math.random() * 100);
+            i=_m0.getPixel(n,r-1);
+            assertNotEquals(4,i);
+            i=_m0.getPixel(n,r+1);
+            assertNotEquals(4,i);
+
         }
 
         @Test
@@ -126,6 +188,21 @@ class MapTest {
 
         @Test
         void fill() {
+            _m0=new Map(100);
+            int r=_m0.getHeight()/2;
+            Index2D p1= new Index2D(0,r);
+            Index2D p2= new Index2D(99,r);
+            _m0.drawLine(p2,p1,4);
+
+            _m0.fill(p,5,true);
+            Index2D p3= new Index2D(90,90);
+            assertEquals(_m0.getPixel(p3),_m0.getPixel(p));
+
+            int n = (int) (Math.random() * 100);
+            int i=_m0.getPixel(n,r);
+            assertEquals(4,i);//line stay the same
+
+
         }
 
         @Test
